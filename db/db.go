abad7e4a239baebae1ae03f56a81d8c094a06bf7
Lukmanul Hakim <manglayangselalu@gmail.com>
2024-05-07 08:50:37 +0700

    update cert

--- db/db.go
+++ db/db.go
@@ -24,13 +24,19 @@ func dbvar() string {
 	DB_NAME := os.Getenv("DB_NAME")
 	DB_USER := os.Getenv("DB_USER")
 	SSL_MODE := os.Getenv("SSL_MODE")
+	sslrootcert := os.Getenv("sslrootcert")
+	sslkey := os.Getenv("sslkey")
+	sslcert := os.Getenv("sslcert")
 
 	dsn := ("host=" + DB_HOST +
 		" user=" + DB_USER +
 		" password=" + DB_PASSWORD +
 		" dbname=" + DB_NAME +
 		" port=" + DB_PORT +
-		" sslmode=" + SSL_MODE) //
+		" sslmode=" + SSL_MODE +
+		" sslrootcert=" + sslrootcert +
+		" sslkey=" + sslkey +
+		" sslcert=" + sslcert) //
 
 	return dsn
 }
